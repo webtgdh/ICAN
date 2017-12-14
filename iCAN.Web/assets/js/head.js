403b6c5d9dfe1ac3bfe54865fe9cb2abe081a28d
kevsimpson <hello@kev-simpson.me>
2017-12-14 10:02:42 +0000

    Initial work on news listing

--- iCAN.Web/assets/js/head.js
+++ iCAN.Web/assets/js/head.js
@@ -1,6 +1,6 @@
 /*!
  * modernizr v3.5.0
- * Build https://modernizr.com/download?-classlist-contains-csstransforms-flexbox-history-setclasses-dontmin
+ * Build https://modernizr.com/download?-classlist-contains-csstransforms-flexbox-flexwrap-history-setclasses-dontmin
  *
  * Copyright (c)
  *  Faruk Ates
@@ -906,6 +906,37 @@ Detects support for the Flexible Box Layout model, a.k.a. Flexbox, which allows 
     return navigator.userAgent.indexOf('Android 2.') === -1 &&
            testAllProps('transform', 'scale(1)', true);
   });
+
+/*!
+{
+  "name": "Flex Line Wrapping",
+  "property": "flexwrap",
+  "tags": ["css", "flexbox"],
+  "notes": [{
+    "name": "W3C Flexible Box Layout spec",
+    "href": "http://dev.w3.org/csswg/css3-flexbox"
+  }],
+  "warnings": [
+    "Does not imply a modern implementation – see documentation."
+  ]
+}
+!*/
+/* DOC
+Detects support for the `flex-wrap` CSS property, part of Flexbox, which isn’t present in all Flexbox implementations (notably Firefox).
+
+This featured in both the 'tweener' syntax (implemented by IE10) and the 'modern' syntax (implemented by others). This detect will return `true` for either of these implementations, as long as the `flex-wrap` property is supported. So to ensure the modern syntax is supported, use together with `Modernizr.flexbox`:
+
+```javascript
+if (Modernizr.flexbox && Modernizr.flexwrap) {
+  // Modern Flexbox with `flex-wrap` supported
+}
+else {
+  // Either old Flexbox syntax, or `flex-wrap` not supported
+}
+```
+*/
+
+  Modernizr.addTest('flexwrap', testAllProps('flexWrap', 'wrap', true));
 
 
   // Run each test
